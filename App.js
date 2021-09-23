5b641e2ca47fe89c1b02c26a28600c21d302b23f
GuztaJF-DS <noobshippuden13@gmail.com>
2021-09-23 11:18:42 -0300

    added The Change Order Option

--- App.js
+++ App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import {
-  View,
+  View, 
   Text,
   SafeAreaView,
   FlatList,
   StyleSheet,
+  Button
 } from 'react-native';
 import List from './src/List';
 import Data from './src/data.json';
 
 export default()=>{
+  const [orderedData,setOrderedData]=useState(false);
+  const [orderBool,setOrderBool]=useState(false);
+    
+  useEffect(()=>{
+      if(orderedData==false){
+        setOrderedData(Data.sort((a,b)=>a.Name.localeCompare(b.Name)));
+        setOrderBool(false)
+      }else if(orderedData==true){
+        setOrderedData(Data.sort((a,b)=>b.Name.localeCompare(a.Name)))
+        setOrderBool(true);
+      }
+  },[orderedData])
+
+
   return (
     <SafeAreaView style={styles.backgroundStyle}>
       <View style={styles.Header}>
           <Text style={{fontSize:18}}>Cursos</Text>
       </View>
+      <Button
+        color="#93c3c7"
+        title="Mudar a Ordem alfabética"
+        onPress={()=>{(orderBool==false)?setOrderedData(true):setOrderedData(false)}}
+      />
       <FlatList
-        data={Data}
+        data={orderedData}
         renderItem={({item})=>(
           <List Name={item.Name} Description={item.Description} ImageUrl={item.ImageUrl}></List>
-          )}
-        keyExtractor={item => item.id}
+        )}
       />
     </SafeAreaView>
   );
